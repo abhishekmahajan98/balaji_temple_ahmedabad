a623f3ad81d21f3722a6e469547162a6acbac54b
sourabh5050 <sourabhpisipati0@gmail.com>
2020-01-07 16:36:58 +0530

    marquee

--- lib/components/main_page.dart
+++ lib/components/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
+import 'package:marquee/marquee.dart';
 import 'home_carousel_images.dart';
 
 class MainPage extends StatefulWidget {
@@ -16,14 +16,24 @@ class _MainPageState extends State<MainPage> {
         Container(
           height: MediaQuery.of(context).size.height / 10,
           width: MediaQuery.of(context).size.width,
-          alignment: Alignment.center,
-          color: Colors.black,
-          child: Text(
-            'Recent Info',
+          color: Color(0xffa62627),
+          child:Marquee(text: 'Some sample text that takes some space.',
             style: TextStyle(
+              fontWeight: FontWeight.bold,
               color: Colors.white,
-              fontSize: 20,
+              fontSize: 30.0,
             ),
+            scrollAxis: Axis.horizontal,
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            blankSpace: 20.0,
+            velocity: 100.0,
+            pauseAfterRound: Duration(seconds: 1),
+            startPadding: 10.0,
+            accelerationDuration: Duration(seconds: 1),
+            accelerationCurve: Curves.linear,
+            decelerationDuration: Duration(milliseconds: 500),
+            decelerationCurve: Curves.easeOut,
           ),
         ),
         SizedBox(
