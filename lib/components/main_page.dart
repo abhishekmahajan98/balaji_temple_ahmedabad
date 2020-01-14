fea01045c1a1480028e025f06632a20cdc043a73
abhishek <abhishek.mahajan314@gmail.com>
2020-01-14 22:43:17 +0530

    home page elements synced from firestore

--- lib/components/main_page.dart
+++ lib/components/main_page.dart
@@ -1,8 +1,11 @@
 import 'package:balaji_temple_ahmedabad/constants.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
 import 'home_carousel_images.dart';
+
+final _firestore = Firestore.instance;
 
 class MainPage extends StatefulWidget {
   @override
@@ -51,69 +54,167 @@ class _MainPageState extends State<MainPage> {
                 child: Column(
                   children: <Widget>[
                     Container(
-                      height: MediaQuery.of(context).size.height / 12,
-                      width: MediaQuery.of(context).size.width,
-                      color: Color(0xffa62627),
-                      alignment: Alignment.center,
-                      child: Marquee(
-                        text: 'Some sample text that takes some space.',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 30.0,
-                        ),
-                        scrollAxis: Axis.horizontal,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        blankSpace: 20.0,
-                        velocity: 100.0,
-                        pauseAfterRound: Duration(seconds: 1),
-                        startPadding: 10.0,
-                        accelerationDuration: Duration(seconds: 1),
-                        accelerationCurve: Curves.linear,
-                        decelerationDuration: Duration(milliseconds: 500),
-                        decelerationCurve: Curves.easeOut,
-                      ),
-                    ),
+                        height: MediaQuery.of(context).size.height / 12,
+                        width: MediaQuery.of(context).size.width,
+                        color: Color(0xffa62627),
+                        alignment: Alignment.center,
+                        child: StreamBuilder(
+                          stream: _firestore
+                              .collection('updates')
+                              .document('marque text')
+                              .snapshots(),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData) {
+                              return Marquee(
+                                text:
+                                    'This is the official application for Sri Balaji Temple, Ahmedabad',
+                                style: kTitleTextStyle,
+                                scrollAxis: Axis.horizontal,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                blankSpace: 20.0,
+                                velocity: 75.0,
+                                //pauseAfterRound: Duration(seconds: 1),
+                                startPadding: 20.0,
+                                //accelerationDuration: Duration(seconds: 1),
+                                //accelerationCurve: Curves.linear,
+                                //decelerationDuration:Duration(milliseconds: 100),
+                                //decelerationCurve: Curves.easeOut,
+                              );
+                            }
+                            final marque_text = snapshot.data['text'];
+                            return Marquee(
+                              text: marque_text,
+                              style: kTitleTextStyle,
+                              scrollAxis: Axis.horizontal,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              blankSpace: 20.0,
+                              velocity: 75.0,
+                              //pauseAfterRound: Duration(seconds: 1),
+                              startPadding: 20.0,
+                              //accelerationDuration: Duration(seconds: 1),
+                              //accelerationCurve: Curves.linear,
+                              //decelerationDuration:Duration(milliseconds: 100),
+                              //decelerationCurve: Curves.easeOut,
+                            );
+                          },
+                        )),
                     SizedBox(
                       height: 5,
                     ),
-                    CarouselSlider.builder(
-                      enlargeCenterPage: true,
-                      enableInfiniteScroll: true,
-                      autoPlay: true,
-                      autoPlayInterval: Duration(seconds: 3),
-                      itemCount: homeCarouselImages.length,
-                      itemBuilder: (BuildContext context, int itemIndex) =>
-                          Container(
-                        height: 200,
-                        child: Image.asset(
-                          homeCarouselImages[itemIndex],
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                    StreamBuilder(
+                      stream:
+                          _firestore.collection('home carousel').snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return CarouselSlider(
+                            height: 200,
+                            enlargeCenterPage: true,
+                            enableInfiniteScroll: true,
+                            autoPlay: true,
+                            autoPlayInterval: Duration(seconds: 1),
+                            items: <Widget>[
+                              Center(
+                                child: CircularProgressIndicator(
+                                  backgroundColor: mainColor,
+                                ),
+                              )
+                            ],
+                          );
+                        }
+                        final images = snapshot.data.documents;
+                        List<Widget> imageList = [];
+                        for (var image in images) {
+                          imageList.add(
+                            Image.network(
+                              image.data['image_url'],
+                              fit: BoxFit.cover,
+                              loadingBuilder: (context, child, progress) {
+                                return progress == null
+                                    ? child
+                                    : LinearProgressIndicator(
+                                        backgroundColor: mainColor,
+                                      );
+                              },
+                            ),
+                          );
+                        }
+                        return CarouselSlider(
+                          height: 200,
+                          enlargeCenterPage: true,
+                          enableInfiniteScroll: true,
+                          autoPlay: true,
+                          autoPlayInterval: Duration(seconds: 3),
+                          items: imageList,
+                        );
+                      },
                     ),
-                    Card(
-                      child: Column(
-                        children: <Widget>[
-                          Material(
-                            color: Color(0xffa62627),
-                            child: ListTile(
-                              title: Center(
-                                child: Text(
-                                  'Next Main Event',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.white,
+                    StreamBuilder(
+                      stream: _firestore
+                          .collection('updates')
+                          .document('upcoming event')
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return Card(
+                            child: Column(
+                              children: <Widget>[
+                                Material(
+                                  color: mainColor,
+                                  child: ListTile(
+                                    title: Center(
+                                      child: CircularProgressIndicator(
+                                        backgroundColor: backgroundColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Center(
+                                    child: CircularProgressIndicator(
+                                      backgroundColor: backgroundColor,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                        final eventName = snapshot.data['event name'];
+                        final eventImageUrl = snapshot.data['image_url'];
+                        return Card(
+                          child: Column(
+                            children: <Widget>[
+                              Material(
+                                color: Color(0xffa62627),
+                                child: ListTile(
+                                  title: Center(
+                                    child: Text(
+                                      eventName,
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
+                              Container(
+                                child: Image.network(
+                                  eventImageUrl,
+                                  fit: BoxFit.cover,
+                                  loadingBuilder: (context, child, progress) {
+                                    return progress == null
+                                        ? child
+                                        : LinearProgressIndicator(
+                                            backgroundColor: mainColor,
+                                          );
+                                  },
+                                ),
+                              )
+                            ],
                           ),
-                          Container(
-                            child: Image.asset('assets/images/balaji4.jpg'),
-                          )
-                        ],
-                      ),
+                        );
+                      },
                     ),
                   ],
                 ))
